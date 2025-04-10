76387811f4058a97db8ebaf3b3f905ac7fd8f8cf
silviaarnoldi <101811166+silviaarnoldi@users.noreply.github.com>
2025-04-10 14:18:20 +0000

    parte2

---
+++ programmaEntrata.php
@@ -0,0 +1,31 @@
+<?php
+include "connessione.php";
+session_start();
+
+?>
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <!--
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     Collegamento al tuo file CSS 
+    <link rel="stylesheet" href="CSS/stylehome.css">
+    <img src="img/logo.png" width="250" height="100"> -->
+</head>
+    <body>
+        <center>
+            <h1>ENTRATA UTENTE:</h1>
+           
+            <br>
+            <form action="entratacontroller.php" method="post">
+        <input type="text" name="nome" placeholder="nome"> <br> <br>
+        <input type="text" name="cognome" placeholder="cognome"> <br> <br>
+        <input type="text" name="persona_da_visitare" placeholder="persona da visitare"> <br> <br>
+        <input type="text" name="qualifica_societa" placeholder="Qualifica e societa' o ente di appartenenza">
+        <input type="text" name="username" placeholder="username"> <br> <br>
+        <input type="text" name="password" placeholder="password">  <br> <br>
+        <input type="submit" value="Registra">
+    </form>
+    <a href="profile.php">Torna alla Home</a>
+    </center>
